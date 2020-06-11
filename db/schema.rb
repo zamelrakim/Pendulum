02623c73a91145228934b0d09cf511af1d27c16e
Zamel Hill <zrhill@gmail.com>
2020-06-11 08:35:59 -0400

    Added Username column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_114351) do
+ActiveRecord::Schema.define(version: 2020_06_11_123327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_114351) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "job_id"
+    t.string "username"
     t.index ["job_id"], name: "index_users_on_job_id"
   end
 

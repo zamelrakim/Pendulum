031c07ede8051a75c1927b84ee21e819f93671c9
Zamel Hill <zrhill@gmail.com>
2020-06-11 07:52:52 -0400

    Updated Associations in Model files

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ApplicationRecord
   has_and_belongs_to_many :tools
+  has_many :employees, class_name: "User", foreign_key: "job_id"
 end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,5 @@
 class Tool < ApplicationRecord
   has_and_belongs_to_many :jobs
+  has_and_belongs_to_many :users
+  belongs_to :creator, class_name: "User", inverse_of: :packages
 end

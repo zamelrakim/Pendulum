48c5546ef1d8c6086a859e0522ef50a16569a335
Zamel Hill <zrhill@gmail.com>
2020-06-11 09:43:54 -0400

    User Auth (Application & Authentication Controllers)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_and_belongs_to_many :tools
   has_many :packages, class_name: "Tool", foreign_key: "creator_id", inverse_of: :creator
   belongs_to :job, optional: true

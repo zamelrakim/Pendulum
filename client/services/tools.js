a4bc4ca2be4671fb48672d738bbb5db8274a40f7
Zamel Hill <zrhill@gmail.com>
2020-06-12 11:49:04 -0400

    Tool Service (Pendulum API)

---
+++ client/services/tools.js
@@ -0,0 +1,21 @@
+import api from './pendulum-api'
+
+export const getTools = async () => {
+  const resp = await api.get('/tools');
+  return resp.data;
+}
+
+export const getTool = async (id) => {
+  const resp = await api.get(`/tools/${id}`);
+  return resp.data;
+}
+
+export const createTool = async (toolData) => {
+  const resp = await api.post('/tools', { tool: toolData });
+  return resp.data;
+}
+
+export const deleteTool = async (id) => {
+  const resp = await api.delete(`/tools/${id}`);
+  return resp
+}

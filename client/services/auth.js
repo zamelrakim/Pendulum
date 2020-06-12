80a5db8290c77313978cfb8cc41ffdad33f9b5f0
Zamel Hill <zrhill@gmail.com>
2020-06-12 11:31:48 -0400

    User Auth (Pendulum API)

---
+++ client/services/auth.js
@@ -0,0 +1,29 @@
+import api from './pendulum-api';
+
+export const loginUser = async (loginData) => {
+  const resp = await api.post('/auth/login', { auth: loginData })
+  localStorage.setItem('token', resp.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
+  return resp.data.user
+}
+
+export const registerUser = async (registerData) => {
+  const resp = await api.post('/users/', { user: registerData })
+  localStorage.setItem('token', resp.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
+  return resp.data.user
+}
+
+export const verifyUser = async () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`
+    const resp = await api.get('/auth/verify');
+    return resp.data
+  }
+  return false
+}
+
+export const removeToken = () => {
+  api.defaults.headers.common.authorization = null;
+}

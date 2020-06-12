5d7376eaf385d4749cd89f077ff256d77fa1705d
Zamel Hill <zrhill@gmail.com>
2020-06-12 11:56:30 -0400

    verifyUser (+Updates - App/React)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { loginUser, registerUser, removeToken, verifyUser } from './services/auth'
 
 export default class App extends Component {
+  state = {
+    currUser: null
+  }
+
+  componentDidMount() {
+    this.verifyUser()
+  }
+
+  verifyUser = async () => {
+    const currUser = await verifyUser()
+    this.setState({ currUser })
+  }
+
   render() {
     return (
       <>

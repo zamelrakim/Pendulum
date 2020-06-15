e59aef80f482d70478faa6c184ff65ac3144534f
Zamel Hill <zrhill@gmail.com>
2020-06-15 08:37:09 -0400

    User Validations (Rails)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tools
   has_many :packages, class_name: "Tool", foreign_key: "creator_id", inverse_of: :creator
   belongs_to :job, optional: true
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
 end

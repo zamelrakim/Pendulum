8b03ff2726d904d36046702508b76d4a7ac8634c
Zamel Hill <zrhill@gmail.com>
2020-06-16 10:15:53 -0400

    Pendulum (Master)

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,4 @@
 class Job < ApplicationRecord
   has_and_belongs_to_many :tools
   has_many :engineers, class_name: "User", foreign_key: "job_id"
-  # accepts_nested_attributes_for :engineers, :tools
 end
